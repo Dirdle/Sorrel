d32deb6e9af49e6280cd48f11815d2b896b62b6b
Dirdle <dirdelius@gmail.com>
2023-03-05 19:41:57 +0000

    First test run with allu, clay, bricks

---
+++ .factorio/mods/sorrel/items/machine-items.lua
@@ -0,0 +1,27 @@
+--machine-items
+
+local datum = {}
+
+local kiln = table.deepcopy(data.raw["item"]["stone-furnace"])
+kiln.name = "kiln"
+kiln.icons = {
+    {
+      icon = kiln.icon,
+      tint = {r=0.9,g=0.9,b=0.9,a=0.3}
+    },
+  }
+kiln.place_result = "kiln"
+datum:extend(kiln)
+
+local brickFurnace = table.deepcopy(data.raw["item"]["stone-furnace"])
+brickFurnace.name = "brick-furnace"
+brickFurnace.icons = {
+    {
+      icon = brickFurnace.icon,
+      tint = {r=0.9,g=0.1,b=0.0,a=0.3}
+    },
+  }
+brickFurnace.place_result = "brick-furnace"
+
+
+data:extend(datum)
